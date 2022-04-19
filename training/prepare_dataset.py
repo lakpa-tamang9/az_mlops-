23ca1ec425ede30e4210932eddabddd859aa6369
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-04-19 14:36:30 +0900

    feat: Interactive authentication

--- training/prepare_dataset.py
+++ training/prepare_dataset.py
@@ -21,7 +21,7 @@ class PrepareDataset:
             # user_assigned_identity = "18557749-e8fa-437a-aa40-dc46986b022b"
             key_vault_name = os.environ["KEY_VAULT_NAME"]
             key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
-            credential = DefaultAzureCredential()
+            credential = DefaultAzureCredential(exclude_interactive_browser_credential=False)
             # credential = DefaultAzureCredential(managed_identity_client_id = user_assigned_identity)
             client = SecretClient(vault_url=key_vault_uri, credential=credential)
         except Exception as error:
