eec8302562c91b9b0ad0582e291ea5f3c54cbddf
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-04-21 14:57:15 +0900

    update comments

--- deployment_setup/deploy_script.py
+++ deployment_setup/deploy_script.py
@@ -3,10 +3,8 @@ from azureml.core.model import Model, InferenceConfig
 from azureml.core.webservice import AciWebservice
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
-import json
 import os
 from azureml.core.authentication import ServicePrincipalAuthentication
-import subprocess
 
 key_vault_name = os.environ["KEY_VAULT_NAME"]
 azure_client_secret = os.environ["AZURE_CLIENT_SECRET"]
@@ -27,15 +25,18 @@ storage_account_key = client.get_secret("ACCOUNT-KEY").value
 service_name_staging = client.get_secret("SERVICE-NAME").value
 
 service_principal = ServicePrincipalAuthentication(
-    tenant_id = azure_tenanat_id,
-    service_principal_id = azure_client_id,
-    service_principal_password = azure_client_secret)
+    tenant_id=azure_tenanat_id,
+    service_principal_id=azure_client_id,
+    service_principal_password=azure_client_secret,
+)
 
-try: 
-    workspace = Workspace(subscription_id = subscription_id, 
-    resource_group = resource_group_name, 
-    workspace_name = workspace_name,
-    auth = service_principal)
+try:
+    workspace = Workspace(
+        subscription_id=subscription_id,
+        resource_group=resource_group_name,
+        workspace_name=workspace_name,
+        auth=service_principal,
+    )
     print("The workspace is created successfully")
 
 except Exception as e:
@@ -43,24 +44,25 @@ except Exception as e:
 
 # Register the model
 try:
-    latest_registered_model = Model.list(workspace)[0]   
+    latest_registered_model = Model.list(workspace)[0]
 except Exception:
     print("No registered model found")
-env = Environment(name = "project_environment")
+env = Environment(name="project_environment")
 
 # Specify the python packages for installing
-python_packages = ['numpy', 'onnxruntime']
+python_packages = ["numpy", "onnxruntime"]
 for package in python_packages:
     env.python.conda_dependencies.add_pip_package(package)
 
 # Specify the inference configuration
 inference_config = InferenceConfig(
-    environment = env,
-    entry_script = "score.py",)
+    environment=env,
+    entry_script="score.py",
+)
 
 # Specify the deployment configuration
 deployment_config = AciWebservice.deploy_configuration(
-cpu_cores=1, memory_gb=1, auth_enabled=True
+    cpu_cores=1, memory_gb=1, auth_enabled=True
 )
 
 # Deploy the service
@@ -75,5 +77,3 @@ service = Model.deploy(
 service.wait_for_deployment(show_output=True)
 key, _ = service.get_keys()
 print(f"Deployment is completed. The service key = {key}")
-
-
