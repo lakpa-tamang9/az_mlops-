3d5b92a136faf6dac9135e6234e91354e9381457
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-04-21 13:26:23 +0900

    feat: Add service principal authentication

--- deployment_setup/deploy_script.py
+++ deployment_setup/deploy_script.py
@@ -5,8 +5,12 @@ from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 import json
 import os
+from azureml.core.authentication import ServicePrincipalAuthentication
 
 key_vault_name = os.environ["KEY_VAULT_NAME"]
+azure_client_secret = os.environ["AZURE_CLIENT_SECRET"]
+azure_tenanat_id = os.environ["AZURE_TENANT_ID"]
+azure_client_id = os.environ["AZURE_CLIENT_ID"]
 key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
 credential = DefaultAzureCredential()
 client = SecretClient(vault_url=key_vault_uri, credential=credential)
@@ -21,12 +25,17 @@ container_name = client.get_secret("CONTAINER-NAME").value
 storage_account_key = client.get_secret("ACCOUNT-KEY").value
 service_name_staging = client.get_secret("SERVICE-NAME").value
 
+service_principal = ServicePrincipalAuthentication(
+    tenant_id = azure_tenanat_id,
+    service_principal_id = azure_client_id,
+    service_principal_password = azure_client_secret)
 
 # Create the workspace with loaded deployment configs
 try: 
     ws = Workspace(subscription_id = subscription_id, 
     resource_group = resource_group_name, 
-    workspace_name = workspace_name)
+    workspace_name = workspace_name,
+    auth = service_principal)
     print("The workspace is created successfully")
 
 except Exception:
@@ -37,7 +46,7 @@ try:
     latest_registered_model = Model.list(ws)[0]   
 except Exception:
     print("No registered model found")
-env = Environment(name="project_environment")
+env = Environment(name = "project_environment")
 
 # Specify the python packages for installing
 python_packages = ['numpy', 'onnxruntime']
@@ -46,15 +55,13 @@ for package in python_packages:
 
 # Specify the inference configuration
 inference_config = InferenceConfig(
-    environment=env,
-    entry_script="score.py",)
+    environment = env,
+    entry_script = "score.py",)
 
 # Specify the deployment configuration
 deployment_config = AciWebservice.deploy_configuration(
 cpu_cores=1, memory_gb=1, auth_enabled=True
 )
-
-# Load the configs containing name for the service name staging
 
 # Deploy the service
 service = Model.deploy(
