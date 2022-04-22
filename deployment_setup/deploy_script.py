3ba43cb8f325447f865a1c88b84c0bd044f150e2
lakpa-tamang9 <Ld.tamang25@gmail.com>
2022-04-22 12:23:49 +0900

    feat: Add arguments from variable group

--- deployment_setup/deploy_script.py
+++ deployment_setup/deploy_script.py
@@ -12,46 +12,50 @@ def main():
     # Get key vault name from the variable group as argument.
     parser = argparse.ArgumentParser(description="Argument for key vault name")
     parser.add_argument("--kv_name", type=str, help="Name of the key vault")
+    parser.add_argument("--az_client_secret", type=str, help="Client secret")
+    parser.add_argument("--az_tenant_ID", type=str, help="Tenant ID")
+    parser.add_argument("--az_client_ID", type=str, help="Client ID")
+    parser.add_argument("--resource_group", type=str, help="Resource group")
+    parser.add_argument("--workspace", type=str, help="ML workspace name")
+    parser.add_argument("--location", type=str, help="Resource group location")
+    parser.add_argument(
+        "--storage_account_name", type=str, help="Name of the storage account"
+    )
+    parser.add_argument("--container_name", type=str, help="Name of the container")
     args = parser.parse_args()
-    key_vault_name = args.kv_name
 
+    key_vault_name = args.kv_name
+    # azure_client_secret = args.az_client_secret
+    # azure_tenanat_id = args.az_tenant_ID
+    # azure_client_id = args.az_client_ID
+    # resource_group = args.resource_group
+    # w_space = args.workspace
+    # location = args.location
+    # storage_account_name = args.storage_account_name
+    # container_name = args.container_name
     print(key_vault_name)
-    key_vault_name = os.environ["KEY_VAULT_NAME"]
-    # azure_client_secret = os.environ["AZURE_CLIENT_SECRET"]
-    # azure_tenanat_id = os.environ["AZURE_TENANT_ID"]
-    # azure_client_id = os.environ["AZURE_CLIENT_ID"]
+    # print(azure_client_secret, azure_tenanat_id, azure_client_id)
+
     key_vault_uri = f"https://{key_vault_name}.vault.azure.net"
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=key_vault_uri, credential=credential)
 
-    # Retrive client secrets
-    azure_client_secret = client.get_secret("AZURE-CLIENT-SECRET").value
-    azure_tenanat_id = client.get_secret("AZURE-TENANT-ID").value
-    azure_client_id = client.get_secret("AZURE-CLIENT-ID").value
-
-    print(azure_client_secret, azure_tenanat_id, azure_client_id)
-
     # Retrieve account and storage details
-    resource_group_name = client.get_secret("RESOURCE-GROUP").value
     subscription_id = client.get_secret("SUB-ID").value
-    location = client.get_secret("LOCATION").value
-    workspace_name = client.get_secret("WORKSPACE").value
-    storage_account_name = client.get_secret("ACCOUNT-NAME").value
-    container_name = client.get_secret("CONTAINER-NAME").value
-    storage_account_key = client.get_secret("ACCOUNT-KEY").value
+    # storage_account_key = client.get_secret("ACCOUNT-KEY").value
     service_name_staging = client.get_secret("SERVICE-NAME").value
 
     service_principal = ServicePrincipalAuthentication(
-        tenant_id=azure_tenanat_id,
-        service_principal_id=azure_client_id,
-        service_principal_password=azure_client_secret,
+        tenant_id=args.az_tenant_ID,
+        service_principal_id=args.az_client_ID,
+        service_principal_password=args.az_client_secret,
     )
 
     try:
         workspace = Workspace(
             subscription_id=subscription_id,
-            resource_group=resource_group_name,
-            workspace_name=workspace_name,
+            resource_group=args.resource_group,
+            workspace_name=args.workspace,
             auth=service_principal,
         )
         print("The workspace is created successfully")
